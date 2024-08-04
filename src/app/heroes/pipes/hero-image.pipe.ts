74ed0d174cbd1e3395fc625d3f08e22c0605238f
Marco33100 <martinezramirezm82@gmail.com>
2024-08-04 13:35:40 -0600

    Funciona todo

--- src/app/heroes/pipes/hero-image.pipe.ts
+++ src/app/heroes/pipes/hero-image.pipe.ts
@@ -10,8 +10,9 @@ export class HeroImagePipe implements PipeTransform {
     if(!hero.id && !hero.alt_img){
       return 'assets/no-image.png'
     }
-    if (hero.alt_img) return hero.alt_img;
-    return `assets/heroes/${hero.id}.jpg`;
+    if (hero.alt_img && hero.alt_img.startsWith('https')) return hero.alt_img;
+    if (hero.alt_img) return `assets/heroes/${hero.alt_img}.jpg`;
+    return "No se ha insertado alguna imagen";
   }
   }
 
